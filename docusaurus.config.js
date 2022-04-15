c68df42118905da3cc14d43703f082c775cfe04e
genshen <genshenchu@gmail.com>
2022-04-15 10:45:24 +0800

    fix math equations display

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,9 +10,13 @@ module.exports = {
   favicon: 'img/favicon.ico',
   organizationName: 'genshen', // Usually your GitHub org/user name.
   projectName: 'blog', // Usually your repo name.
-  stylesheets: [
-    'https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css',
-  ],
+  stylesheets: [{
+    href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+    type: 'text/css',
+    integrity:
+      'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+    crossorigin: 'anonymous',
+  }],
   themeConfig: {
     prism: {
       additionalLanguages: ['rust'],
