a2b2f8a969f2f1b3d500b902599721e2142c2fc5
genshen <genshenchu@gmail.com>
2022-07-20 15:24:48 +0800

    bump docusaurus to 2.0.0-rc.1 from scratch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,24 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
 const math = require('remark-math')
 const katex = require('rehype-katex')
 
-module.exports = {
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: 'Blog',
-  tagline: 'The tagline of my blog site',
+  tagline: 'Blog site of gensh.me',
   url: 'https://gensh.me',
   baseUrl: '/',
   onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'genshen', // Usually your GitHub org/user name.
   projectName: 'blog', // Usually your repo name.
   stylesheets: [{
@@ -17,102 +28,100 @@ module.exports = {
       'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
     crossorigin: 'anonymous',
   }],
-  themeConfig: {
-    prism: {
-      additionalLanguages: ['rust'],
-    },
-    navbar: {
-      title: 'Blog',
-      logo: {
-        alt: 'Blog Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        // {to: 'docs/doc1', label: 'Docs', position: 'left'},
-        {to: '/', label: 'Blog', position: 'left'},
-        {to: '/tags', label: 'Tags', position: 'left'},
-        {to: 'about', label: 'About', position: 'left'},
-        {
-          href: 'https://github.com/genshen',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        // {
-        //   title: 'Docs',
-        //   items: [
-        //     {
-        //       label: 'Style Guide',
-        //       to: 'docs/doc1',
-        //     },
-        //     {
-        //       label: 'Second Doc',
-        //       to: 'docs/doc2',
-        //     },
-        //   ],
-        // },
-        // {
-        //   title: 'Community',
-        //   items: [
-        //     {
-        //       label: 'Stack Overflow',
-        //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-        //     },
-        //     {
-        //       label: 'Discord',
-        //       href: 'https://discordapp.com/invite/docusaurus',
-        //     },
-        //   ],
-        // },
-        {
-          title: 'Social',
-          items: [
-            {
-              label: 'Blog',
-              to: '/',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/genshen',
-            }
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} gensh.me`,
-    },
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
   },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        // docs: {
-        //   path: 'docs',
-        //   sidebarPath: require.resolve('./sidebars.js'),
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/edit/master/website/',
-        // },
-        docs: false,
-        blog: {
-          path: './blog',
-          routeBasePath: '/',
-          postsPerPage: 3,
-          blogDescription: 'A docusaurus powered blog!',
-          // https://spectrum.chat/unified/remark/remark-math-on-docusaurus-v2~220079aa-2dab-4d2e-b39a-a33563107dc5
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
-          feedOptions: {
-            type: 'all',
-            copyright: `Copyright © ${new Date().getFullYear()} gensh.me.`,
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Blog',
+        logo: {
+          alt: 'Blog Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          // {
+          //   type: 'doc',
+          //   docId: 'intro',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
+          {to: '/', label: 'Blog', position: 'left'},
+          {to: '/tags', label: 'Tags', position: 'left'},
+          {to: 'about', label: 'About', position: 'left'},        {
+            href: 'https://github.com/genshen',
+            label: 'GitHub',
+            position: 'right',
           },
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
+        ],
       },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Social',
+            items: [
+              {
+                label: 'Blog',
+                to: '/',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/genshen',
+              }
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} gensh.me.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['rust'],
+      },
+    }),
+    presets: [
+      [
+        'classic',
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          // docs: {
+          //   sidebarPath: require.resolve('./sidebars.js'),
+          //   // Please change this to your repo.
+          //   // Remove this to remove the "edit this page" links.
+          //   editUrl:
+          //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // },
+          docs: false,
+          blog: {
+            showReadingTime: true,
+            routeBasePath: '/',
+            postsPerPage: 3,
+            blogDescription: 'A docusaurus powered blog!',
+            // https://spectrum.chat/unified/remark/remark-math-on-docusaurus-v2~220079aa-2dab-4d2e-b39a-a33563107dc5
+            remarkPlugins: [math],
+            rehypePlugins: [katex],
+            // Please change this to your repo.
+            // Remove this to remove the "edit this page" links.
+            editUrl:
+              'https://github.com/genshen/blog-site/tree/master/',
+            feedOptions: {
+              type: 'all',
+              copyright: `Copyright © ${new Date().getFullYear()} gensh.me.`,
+            },
+          },
+          theme: {
+            customCss: require.resolve('./src/css/custom.css'),
+          },
+        }),
+      ],
     ],
-  ],
 };
+
+module.exports = config;
